2c5f02d4eb7c8c0a349963a6b4d3ba388786fdfd
Lucas Rodrigo <Lucas.moreira34@fatec.sp.gov.br>
2022-10-31 20:36:46 -0300

    Update

---
+++ 4º Semestre/Eletiva Programacao de Scripts/src/components/Header/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const WrapperSld = styled.div`
+border-bottom: 1px solid #bbb;
+display: flex;
+padding: 10px 0px;
+
+`;
+
+export const ResultadoSld = styled.div`
+    font-size: 30px;
+    color #0066b3;
+    font-weight: bold;
+    
+`;
+
+export const ConcursoSld = styled.div`
+font-size: 20px;
+color #aaa;
+display: flex;
+align-items: flex-end;
+padding-bottom: 3px;
+margin-left: 10px;
+`;
